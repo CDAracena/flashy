538a2703dc95969d91dc29e2c22e3612ae9a33ca
Christian Aracena <CDAracena@gmail.com>
2018-09-03 20:54:59 -0400

    started setting up certain variables and got toastr js for notifications

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  
+
+let IncompleteArray = [];
+let CompletedArray = [];
+
+let frontCardText = document.querySelector(".flash-card-col")
+if (IncompleteArray.length === 0){
+  frontCardText.innerText = "Looks like you haven't added any cards yet"
+}
+
+
+
 })
