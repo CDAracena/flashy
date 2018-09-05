769662b517c14ad8a1ca6272ce5d4e8b1a27ed6d
Christian Aracena <CDAracena@gmail.com>
2018-09-04 20:35:04 -0400

    JS functions to add new flash card object to incomplete array, modal added

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 $(document).ready(function(){
 
+let collectingDataArray = [];
 let IncompleteArray = [];
 let CompletedArray = [];
+let randomCard = Math.floor(Math.random() * IncompleteArray.length)
 
-let frontCardText = document.querySelector(".flash-card-col")
+let cardTitle = document.querySelector('.card-title-input')
+let cardDefinition = document.querySelector('.card-description-input');
+let statusMessage = document.querySelector('.status-message');
+let addCardBtn = document.querySelector('add-card-button');
+
 if (IncompleteArray.length === 0){
-  frontCardText.innerText = "Looks like you haven't added any cards yet"
+  statusMessage.innerText = "Looks like you haven't added any cards yet...";
 }
 
+let addFlashCardBtn = document.querySelector('.add-button');
+
+addFlashCardBtn.addEventListener('click', function(){
+  $('.modal').fadeIn('slow')
+})
+
+$('.close-modal-icon').click(function(){
+  $('.modal').fadeOut('slow');
+})
+
+$('.cancel-btn').click(function(){
+  $('.modal').fadeOut('slow');
+})
+
+
+$('.add-card-button').click(function(){
+  IncompleteArray.push({fcTitle: cardTitle.value, fcDescription: cardDefinition.value});
+console.log(IncompleteArray)
+$('.modal').fadeOut('slow');
+})
 
 
 })
