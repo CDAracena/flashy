f41c25aee465f012fb9a751a63f27d52b7c4120f
Christian Aracena <CDAracena@gmail.com>
2018-09-05 22:36:42 -0400

    jquery dom manipulation, toastr msgs on add card

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-let collectingDataArray = [];
-let IncompleteArray = [];
-let CompletedArray = [];
-let randomCard = Math.floor(Math.random() * IncompleteArray.length)
+  let IncompleteArray = [];
+  let CompletedArray = [];
+  let randomCard = Math.floor(Math.random() * IncompleteArray.length)
+  let cardTitle = document.querySelector('.card-title-input')
+  let cardDefinition = document.querySelector('.card-description-input');
 
-let cardTitle = document.querySelector('.card-title-input')
-let cardDefinition = document.querySelector('.card-description-input');
-let statusMessage = document.querySelector('.status-message');
-let addCardBtn = document.querySelector('add-card-button');
+  if (IncompleteArray.length === 0) {
+    $('.status-message').text("Looks like you haven't added any cards yet...");
+  }
 
-if (IncompleteArray.length === 0){
-  statusMessage.innerText = "Looks like you haven't added any cards yet...";
-}
+  let addFlashCardBtn = document.querySelector('.add-button');
 
-let addFlashCardBtn = document.querySelector('.add-button');
+  addFlashCardBtn.addEventListener('click', function() {
+    $('.modal').fadeIn('slow')
+  })
 
-addFlashCardBtn.addEventListener('click', function(){
-  $('.modal').fadeIn('slow')
-})
+  $('.close-modal-icon').click(function() {
+    $('.modal').fadeOut('slow');
+  })
 
-$('.close-modal-icon').click(function(){
-  $('.modal').fadeOut('slow');
-})
+  $('.cancel-btn').click(function() {
+    $('.modal').fadeOut('slow');
 
-$('.cancel-btn').click(function(){
-  $('.modal').fadeOut('slow');
-})
+  })
 
+// Adding a new card function starts here
 
-$('.add-card-button').click(function(){
-  IncompleteArray.push({fcTitle: cardTitle.value, fcDescription: cardDefinition.value});
-console.log(IncompleteArray)
-$('.modal').fadeOut('slow');
-})
-
-
+  $('.add-card-button').click(function() {
+    if (cardTitle.value && cardDefinition.value) {
+      IncompleteArray.push({fcTitle: cardTitle.value, fcDescription: cardDefinition.value});
+      console.log(IncompleteArray)
+      $('.modal').fadeOut('slow');
+      $('.status-message').text("Looks like you added some cards! Feel free to add more..or start")
+      toastr.success(`${cardTitle.value} has been added!`)
+      $('.card-title-input').val('');
+      $('.card-description-input').val('');
+    } else {
+      toastr.error('Please make sure to fill out card title or card description')
+    }
+  })
+// Adding a new card function ends here
 })
