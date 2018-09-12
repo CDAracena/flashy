1fd1f24c94752a882eda486af19a8d5f94cb817a
Christian Aracena <CDAracena@gmail.com>
2018-09-11 20:24:53 -0400

    timer renders random flash card onto main upon starting

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ $(document).ready(function() {
 
   })
 
-function addCard(){
-  let row = document.querySelector('.incomplete-col-container')
+  function addCard() {
+    let row = document.querySelector('.incomplete-col-container')
     let col = document.createElement('div');
     col.classList.add('flash-card-item');
-    col.innerText = IncompleteArray[IncompleteArray.length -1].fcTitle;
+    col.innerText = IncompleteArray[IncompleteArray.length - 1].fcTitle;
     row.appendChild(col)
 
-}
+  }
 
-// Adding a new card function starts here
+  // Adding a new card function starts here
 
   $('.add-card-button').click(function() {
     if (cardTitle.value && cardDefinition.value) {
@@ -53,28 +53,32 @@ function addCard(){
       toastr.error('Please make sure to fill out card title or card description')
     }
   })
-// Adding a new card function ends here
+  // Adding a new card function ends here
 
+  //Start Quiz function starts here
+  $('.start-btn').click(function() {
+    $('.timer-div').css({"display": "block"});
+    $('.flag-container').fadeOut('slow');
+    $('.add-btn-container').removeClass('col-lg-4').addClass('col-lg-8');
 
-//Start Quiz function starts here
-$('.start-btn').click(function(){
-  randomCard = Math.floor(Math.random() * IncompleteArray.length)
-  $('.timer-div').css({"display": "block"});
-  $('.flag-container').css({"display": "none"});
-  $('.add-btn-container').css({"display": "none"})
+    var timer = new Timer();
 
-  var timer = new Timer();
+    timer.start({
+      countdown: true,
+      startValues: {
+        seconds: 20
+      }
+    });
+    $('.timer-div').html(timer.getTimeValues().toString());
+    randomCard = Math.floor(Math.random() * IncompleteArray.length)
+    $('.add-btn-container').html(IncompleteArray[randomCard].fcTitle);
 
-  timer.start({countdown: true, startValues: {seconds: 20}});
-  $('.timer-div').html(timer.getTimeValues().toString());
-
-  timer.addEventListener('secondsUpdated', function (e) {
-  $('.timer-div').html(timer.getTimeValues().toString());
-  });
-  timer.addEventListener('targetAchieved', function (e) {
-  $('.timer-div').html('Times Up');
-  });
-})
-
+    timer.addEventListener('secondsUpdated', function(e) {
+      $('.timer-div').html(timer.getTimeValues().toString());
+    });
+    timer.addEventListener('targetAchieved', function(e) {
+      $('.timer-div').html('Times Up');
+    });
+  })
 
 })
